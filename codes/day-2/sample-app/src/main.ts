7f5145b4f71c4805e8aac9ddf4c70459a7db4d31
Joydip <joydip.mondal@gmail.com>
2023-07-20 12:41:39 +0530

    sample app

--- codes/day-2/sample-app/src/main.ts
+++ codes/day-2/sample-app/src/main.ts
@@ -1,7 +1,17 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgModuleRef, PlatformRef } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+setTimeout(
+  () => {
+    const platform: PlatformRef = platformBrowserDynamic()
+    const p = platform.bootstrapModule(AppModule)
+    p.then(
+      (ref: NgModuleRef<AppModule>) => {
+        console.log(ref.instance)
+      },
+      (e) => { console.log(e) }
+    )
+  },
+  5000
+)
